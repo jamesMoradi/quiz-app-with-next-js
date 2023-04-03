4791754a1e600c84bad3722a3abb27f4903a73a6
james moradi <james.js.dev@outlook.com>
2023-04-03 06:53:34 -0700

    controllers created

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,47 @@
-import Link from 'next/link'
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import Questions from '@/layout/Questions'
 
 const Home = () => {
+  const [questionNumber, setQuestionNumber] = useState(1)
+  const [isBegan, setIsBegan] = useState(false)
+
+  const beginController = () => {
+    if (questionNumber > 0) {
+      setIsBegan(true)
+    } else if (questionNumber === 0){
+      setIsBegan(false)
+    }
+
+  }
+
+  const beginHandler = () => {
+    if (questionNumber > 0 && questionNumber < 10) {
+      setQuestionNumber(prev => prev += 1)
+    }
+    beginController()
+    console.log({questionNumber,isBegan});
+  }
+
+  const prevHnadler = () => {
+    if (questionNumber > 0) {
+      setQuestionNumber(prev => prev -= 1)
+    }
+    beginController
+    console.log({questionNumber,isBegan});
+  }
+
   return (
     <section>
-      <p>welcome to react.js test for begginers</p>
-      <p>press the start button to begin</p>
-      <button>
-        <Link href='./quiz/'>start the test</Link>
-      </button>
+      <div>
+        <p>welcome to react.js test for begginers</p>
+        <p>press the start button to begin</p>
+      </div>
+      {isBegan && <Questions questionNumber={questionNumber}/>}
+      <div>
+        <button onClick={beginHandler}>{isBegan ? 'start the test' : 'next'}</button>
+        {isBegan && <button onClick={prevHnadler}>previous</button>}  
+      </div>
     </section>
   )
 }
